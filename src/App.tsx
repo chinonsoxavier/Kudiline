3a14552b4e99765406812c8301e355f201991dda
daffydanni <daffydanni@gmail.com>
2025-09-07 17:46:10 +0100

    start-up

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div className='text-4xl font-bold text-green-500 text-center mt-20'>
+      <h1>Welcome to Kudiline!</h1>
+    </div>
+  )
+}
+
+export default App
